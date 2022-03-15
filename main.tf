c8a6c02f442d737484a962f02eb9c3d63ab833cb
sunnyMunna <sunny.ahmed@bpmwave.com>
2022-03-15 16:27:43 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,9 +3,10 @@ provider "aws" {
 }
 
 module "s3-webapp" {
-  source  = "app.terraform.io/hashicorp-learn/s3-webapp/aws"
+  source  = "app.terraform.io/CloudChemists/s3-webapp/aws"
+  version = "1.0.0"
   name        = var.name
   region = var.region
   prefix = var.prefix
-  version = "1.0.0"
+  # insert required variables here
 }
